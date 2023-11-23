c2e725ee485b972541cad88d3245be2d4a35d9dd
yatahyde <yasmimmafort@gmail.com>
2023-11-23 10:35:20 -0300

    GNL + BONUS

--- Get_Next_Line/get_next_line_bonus.h
+++ Get_Next_Line/get_next_line_bonus.h
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yathayde <yasmimmafort@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 12:52:41 by yathayde          #+#    #+#             */
-/*   Updated: 2023/11/14 12:52:44 by yathayde         ###   ########.fr       */
+/*   Created: 2023/11/23 10:20:38 by yathayde          #+#    #+#             */
+/*   Updated: 2023/11/23 10:20:53 by yathayde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <limits.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
@@ -23,7 +24,7 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *string, int searchedChar );
+char	*ft_strchr(const char *string, int searchedChar);
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t elementCount, size_t elementSize);
